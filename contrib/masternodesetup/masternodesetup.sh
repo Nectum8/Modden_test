da52005a4fe87fedbf378cbb7e97fb96952a8d4c
ModdenBF <108405441+ModdenBF@users.noreply.github.com>
2022-07-05 14:55:30 -0400

    Update masternodesetup.sh

--- contrib/masternodesetup/masternodesetup.sh
+++ contrib/masternodesetup/masternodesetup.sh
@@ -145,7 +145,7 @@ function update_config() {
   cat << EOF >> $CONFIGFOLDER/$CONFIG_FILE
 logintimestamps=1
 maxconnections=256
-#bind=$NODEIP
+bind=0.0.0.0
 masternode=1
 externalip=$NODEIP:$COIN_PORT
 masternodeprivkey=$COINKEY
